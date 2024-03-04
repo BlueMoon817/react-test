7172271c24eefc9147da28682bc825e862694e5c
yr <yr817@hivelab.co.kr>
2024-03-04 12:38:49 +0900

    common tab

--- src/common.js
+++ src/common.js
@@ -2,24 +2,30 @@
 function TabFunc(){
   window.TEST= window.TEST || {};
   window.TEST.Tab=function(){
-    let arr=[];
-    
-    let tabList = document.querySelectorAll('.js-tab-list');
-    tabList.forEach((listItem, index)=>{
-      arr.push({ "id": index, "tab": listItem.parentNode, "list":[], "panel":[]});
-      arr[index].tab.querySelectorAll('.js-tab-panel').forEach(item =>{
-        arr[index].panel.push(item);
-      });
-      listItem.querySelectorAll('.js-tab-item').forEach(li=>{
-        arr[index].list.push(li);
-      })
+    let tabArr = document.querySelectorAll('.tab');
+    let tabListArr = [], tabPanelArr=[];
+
+    tabArr.forEach((tab)=>{
+      let panelEl = tab.querySelectorAll('.js-tab-panel');
+      let ulEl = tab.querySelectorAll('.js-tab-item');
+      tabPanelArr.push(panelEl);
+      tabListArr.push(ulEl);
     });
-    arr.forEach((ob,index)=>{
-      ob.tab.addEventListener('click',function(ev){
-        ob.list.forEach((li,idx)=>{
-          ev.target.length==li ? li.classList.add('is-selected') : li.classList.remove('is-selected')
-        })
-      })
+    tabArr.forEach((tab, index) => {
+      tab.addEventListener('click', function(ev){
+        let liEl = ev.target.closest('LI') ? ev.target.closest('LI'):false;
+        if (liEl){
+          tabListArr[index].forEach((li,idx)=>{
+            if(liEl===li){
+              tabPanelArr[index][idx].classList.add('is-active');
+              li.classList.add('is-selected');
+            }else{
+              li.classList.remove('is-selected');
+              tabPanelArr[index][idx].classList.remove('is-active');
+            }
+          });
+        }
+      });
     });
   }
 }
--- src/components/tab.jsx
+++ src/components/tab.jsx
@@ -14,13 +14,13 @@ const Tab = () => {
           <button type="button" className="tab-btn js-tab-btn" role="tab" aria-selected="true" aria-controls="tab03">Tab 03</button>
         </li>
       </ul>
-      <div className="js-tab-panel tab-panel" role="tabpanel" id="tab01">
-        Tab contents 01
+      <div className="js-tab-panel tab-panel is-active" role="tabpanel" id="tab01">
+        Tab contents 01 
       </div>
-      <div className="js-tab-panel tab-panel" role="tabpanel" id="tab02" style={{display:'none'}}>
+      <div className="js-tab-panel tab-panel" role="tabpanel" id="tab02">
         Tab contents 02
       </div>
-      <div className="js-tab-panel tab-panel" role="tabpanel" id="tab03" style={{display:'none'}}>
+      <div className="js-tab-panel tab-panel" role="tabpanel" id="tab03">
         Tab contents 03
       </div>
     </div>
